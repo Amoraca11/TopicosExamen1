c9d4bf37bec2095704c2d6da823f542415ae11e1
rogeromar <roger@DESKTOP-DP6N0PK>
2016-10-17 12:26:35 -0600

    Cambio de no me acuerdo

--- TopicosExamen1/TopicosExamen1/SolitaDatos.Designer.cs
+++ TopicosExamen1/TopicosExamen1/SolitaDatos.Designer.cs
@@ -128,6 +128,7 @@
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(100, 20);
             this.numericUpDown1.TabIndex = 10;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
             // 
             // SolitaDatos
             // 
--- TopicosExamen1/TopicosExamen1/SolitaDatos.cs
+++ TopicosExamen1/TopicosExamen1/SolitaDatos.cs
@@ -21,5 +21,10 @@ namespace TopicosExamen1
         {
             this.Close();
         }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
