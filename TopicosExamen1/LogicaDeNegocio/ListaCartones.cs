05154bd0b5fd903bbddbbf49f46445c1e0d07987
Amoraca11 <andresmoraca_11@hotmail.com>
2016-10-20 12:17:23 -0600

    Se agrega carton y lista cartones

--- TopicosExamen1/LogicaDeNegocio/ListaCartones.cs
+++ TopicosExamen1/LogicaDeNegocio/ListaCartones.cs
@@ -1,13 +1,39 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace LogicaDeNegocio
 {
     class ListaCartones
     {
+
+       public static ArrayList listaCartones = new ArrayList();
+
+
+        public static void AgregarCarton(Carton CartonAgregar) {
+
+            
+            listaCartones.Add(CartonAgregar);
+             
+   
+
+        }
+
+        public static void ImprimeLista() {
+
+            foreach (var item in listaCartones)
+            {
+                Console.WriteLine("Imprime " + item);
+            }
+
+        }
+        
+
         public int idUsuario { get; set; }
 
         public int  idCarton{ get; set; }
