e74cb2b113b2211c3c646adc46ec230ad8ecceb2
rogeromar11 <roger@DESKTOP-DP6N0PK>
2016-10-20 17:05:00 -0600

    se agrega la clase usuario

--- TopicosExamen1/LogicaDeNegocio/Usuario.cs
+++ TopicosExamen1/LogicaDeNegocio/Usuario.cs
@@ -8,8 +8,58 @@ namespace LogicaDeNegocio
 {
     public class Usuario
     {
-        string nombre;
-        int id;
-        int[] cartones;
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public int cantidad { get; set; }
+        public int[,] cartones { get; set; }
+
+        public Usuario(int Id, string nombreU, int Cantidad, int[,] Numcarton)
+        {
+            id = Id;
+            nombre = nombreU;
+            cantidad = Cantidad;
+            cartones = Numcarton;
+        }
+
+        public int getID()
+        {
+            return this.id;
+        }
+
+        public void setID(int idNuevo)
+        {
+            this.id = idNuevo;
+        }
+
+        public void setNombre(string Nombre)
+        {
+            this.nombre = Nombre;
+        }
+
+        public string getnombre()
+        {
+            return this.nombre;
+        }
+
+        public int getCantidad()
+        {
+            return this.cantidad;
+        }
+
+        public void setcantidad(int Cantidad)
+        {
+            this.cantidad = Cantidad;
+        }
+
+        public int[,] getCartones()
+        {
+            return this.cartones;
+        }
+
+        public void setCarton(int[,] CartonNum)
+        {
+            this.cartones = CartonNum;
+        }
+
     }
 }
