d6f9f3ce99386556cfc05b09b4bfed4c852e6330
rogeromar11 <roger@DESKTOP-DP6N0PK>
2016-10-20 11:36:59 -0600

    Se modifica la interfaz de solicitar datos

--- TopicosExamen1/TopicosExamen1/SolitaDatos.Designer.cs
+++ TopicosExamen1/TopicosExamen1/SolitaDatos.Designer.cs
@@ -30,14 +30,10 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.txtPrimerNum = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.label5 = new System.Windows.Forms.Label();
-            this.txtSegundoNum = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -55,21 +51,11 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 98);
+            this.label2.Location = new System.Drawing.Point(231, 41);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(72, 18);
             this.label2.TabIndex = 1;
             this.label2.Text = "Cartones";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(197, 119);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(158, 18);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Cantidad de números";
             // 
             // textBox1
             // 
@@ -78,16 +64,9 @@
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 5;
             // 
-            // txtPrimerNum
-            // 
-            this.txtPrimerNum.Location = new System.Drawing.Point(361, 120);
-            this.txtPrimerNum.Name = "txtPrimerNum";
-            this.txtPrimerNum.Size = new System.Drawing.Size(50, 20);
-            this.txtPrimerNum.TabIndex = 7;
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(151, 172);
+            this.button1.Location = new System.Drawing.Point(133, 131);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 8;
@@ -97,7 +76,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(270, 172);
+            this.button2.Location = new System.Drawing.Point(271, 131);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 9;
@@ -107,41 +86,20 @@
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(80, 119);
+            this.numericUpDown1.Location = new System.Drawing.Point(308, 65);
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(100, 20);
             this.numericUpDown1.TabIndex = 10;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(418, 126);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(15, 13);
-            this.label5.TabIndex = 11;
-            this.label5.Text = "al";
-            this.label5.Click += new System.EventHandler(this.label5_Click);
-            // 
-            // txtSegundoNum
-            // 
-            this.txtSegundoNum.Location = new System.Drawing.Point(439, 120);
-            this.txtSegundoNum.Name = "txtSegundoNum";
-            this.txtSegundoNum.Size = new System.Drawing.Size(50, 20);
-            this.txtSegundoNum.TabIndex = 12;
             // 
             // SolitaDatos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(501, 224);
-            this.Controls.Add(this.txtSegundoNum);
-            this.Controls.Add(this.label5);
+            this.ClientSize = new System.Drawing.Size(493, 189);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.txtPrimerNum);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "SolitaDatos";
@@ -156,13 +114,9 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox txtPrimerNum;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox txtSegundoNum;
     }
 }
