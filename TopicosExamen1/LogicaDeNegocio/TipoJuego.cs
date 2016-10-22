cb436a64df7d096d33071e05879f284fa4399781
Yusef Ruiz Morales <Yusef Ruiz Morales@YUSEFPORTABLE>
2016-10-21 21:45:09 -0600

    terminando tipo Juegp

--- TopicosExamen1/LogicaDeNegocio/TipoJuego.cs
+++ TopicosExamen1/LogicaDeNegocio/TipoJuego.cs
@@ -23,7 +23,8 @@ namespace LogicaDeNegocio
         public static void ImrimeJuego() {
             for (int i = 0; i < 5; i++) {
                 for (int j = 0; j < 5; j++) {
-                    Console.Write( matriz[i,j]);
+               //     var result = matriz[i, j];
+                   Console.Write(result );
                 }
 
             }
